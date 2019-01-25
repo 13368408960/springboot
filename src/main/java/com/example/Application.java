f442ebdbf698a32ad5c61d114b19bd3a931d325c
lichao <522084578@qq.com>
2019-01-25 14:56:16 +0800

    修改boot版本

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,11 +1,8 @@
 package com.example;
 
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.tomcat.jdbc.pool.DataSource;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -17,37 +14,14 @@ import org.springframework.transaction.PlatformTransactionManager;
 //@EnableScheduling
 @SpringBootApplication
 @MapperScan("com.example.mapper")
+@Slf4j
 public class Application {
 	
-	protected static Logger logger=LoggerFactory.getLogger(Application.class);
 
-	@Bean
-    @ConfigurationProperties(prefix="spring.datasource")
-    public DataSource dataSource(){
-        return new org.apache.tomcat.jdbc.pool.DataSource();
-    }
-
-    @Bean
-    public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
-
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(dataSource());
-
-        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-
-        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/mybatis/*.xml"));
-
-        return sqlSessionFactoryBean.getObject();
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(dataSource());
-    }
 
     
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		logger.info("SpringBoot Start Success");
+		log.info("SpringBoot Start Success");
 	}
 }
--- src/main/java/com/example/aop/LogAspect.java
+++ src/main/java/com/example/aop/LogAspect.java
@@ -1,6 +1,7 @@
 package com.example.aop;
 
-import org.aspectj.lang.JoinPoint;  
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;  
 import org.aspectj.lang.annotation.*;  
 import org.springframework.stereotype.Component;  
@@ -15,8 +16,9 @@ import java.util.Arrays;
  * 日志切面 
  */  
 @Aspect  
-@Component  
-public class LogAspect {  
+@Component
+@Slf4j
+public class LogAspect {
     @Pointcut("execution(public * com.example.service.*.*(..))")  
     public void webLog(){}  
   
@@ -25,40 +27,40 @@ public class LogAspect {
         // 接收到请求，记录请求内容  
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();  
         HttpServletRequest request = attributes.getRequest();  
-        // 记录下请求内容  
-        System.out.println("URL : " + request.getRequestURL().toString());  
-        System.out.println("HTTP_METHOD : " + request.getMethod());  
-        System.out.println("IP : " + request.getRemoteAddr());  
-        System.out.println("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());  
-        System.out.println("ARGS : " + Arrays.toString(joinPoint.getArgs()));  
+        // 记录下请求内容
+        log.info("URL : " + request.getRequestURL().toString());
+        log.info("HTTP_METHOD : " + request.getMethod());
+        log.info("IP : " + request.getRemoteAddr());
+        log.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        log.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
   
     }  
   
     @AfterReturning(returning = "ret", pointcut = "webLog()")  
     public void doAfterReturning(Object ret) throws Throwable {  
         // 处理完请求，返回内容  
-        System.out.println("方法的返回值 : " + ret);  
+        log.info("方法的返回值 : " + ret);
     }  
   
     //后置异常通知  
     @AfterThrowing("webLog()")  
-    public void throwss(JoinPoint jp){  
-        System.out.println("方法异常时执行.....");  
+    public void throwss(JoinPoint jp){
+        log.info("方法异常时执行.....");
     }  
   
     //后置最终通知,final增强，不管是抛出异常或者正常退出都会执行  
     @After("webLog()")  
-    public void after(JoinPoint jp){  
-        System.out.println("方法最后执行.....");  
+    public void after(JoinPoint jp){
+        log.info("方法最后执行.....");
     }  
   
     //环绕通知,环绕增强，相当于MethodInterceptor  
     @Around("webLog()")  
-    public Object arround(ProceedingJoinPoint pjp) {  
-        System.out.println("方法环绕start.....");  
+    public Object arround(ProceedingJoinPoint pjp) {
+        log.info("方法环绕start.....");
         try {  
-            Object o =  pjp.proceed();  
-            System.out.println("方法环绕proceed，结果是 :" + o);  
+            Object o =  pjp.proceed();
+            log.info("方法环绕proceed，结果是 :" + o);
             return o;  
         } catch (Throwable e) {  
             e.printStackTrace();  
